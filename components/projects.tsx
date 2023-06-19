c2a21c64cb419ab302a674764cc046c16dd6c922
anton palermo <anpbusinessmail@gmail.com>
2023-06-19 23:48:39 +0800

    added projects sample covers

--- components/projects.tsx
+++ components/projects.tsx
@@ -1,5 +1,8 @@
 'use client'
 
+import { Card } from '@components/ui/card'
+
+import Image from 'next/image'
 import ShellContainer from '@components/shell-container'
 
 export default function Projects() {
@@ -7,6 +10,17 @@ export default function Projects() {
     <ShellContainer>
       <h1 className="mb-2 text-lg font-bold">Projects</h1>
       <p className="leading-relaxed">Collection of my awesome projects</p>
+      <div className="grid grid-cols-2 gap-3 mt-5">
+        <Card>
+          <Image
+            src="https://res.cloudinary.com/dbndyylmv/image/upload/v1687189039/covers/rick_and_morty_app_ny33iz.png"
+            alt="Rick and Morty Cover Image"
+            width={200}
+            height={200}
+          />
+        </Card>
+        <Card></Card>
+      </div>
     </ShellContainer>
   )
 }
