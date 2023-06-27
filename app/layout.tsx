3b906e18cf7c8ee8ff9412b15c212e72b9fe354b
anton palermo <anpbusinessmail@gmail.com>
2023-06-27 21:13:12 +0800

    style: update expirence layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,8 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'anpalermo',
-  description: 'Personal portfolio homepage'
+  title: 'antonpalermo',
+  description: 'Personal portfolio webpage'
 }
 
 export default function RootLayout({
--- components/experience.tsx
+++ components/experience.tsx
@@ -7,14 +7,18 @@ export interface ExperienceProps {
 export default function Experience({ experience }: ExperienceProps) {
   return (
     <div>
-      <span className="text-sm text-slate-500">{experience.duration}</span>
+      <span className="text-sm font-medium text-slate-500">
+        {experience.duration}
+      </span>
       <div className="mb-3">
         <h2 className="text-base font-semibold text-gray-900">
           {experience.title}
         </h2>
-        <p className="text-sm text-slate-500">{experience.company}</p>
+        <p className="text-sm font-medium text-slate-500">
+          {experience.company}
+        </p>
       </div>
-      <p className="text-slate-500">{experience.description}</p>
+      <p className="line-clamp-2 text-slate-500">{experience.description}</p>
     </div>
   )
 }
