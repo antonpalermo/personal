04730fa4603d138ba623c6bb33e5b5bf5e5cdc3b
anton palermo <anpbusinessmail@gmail.com>
2023-06-28 22:38:50 +0800

    refac: removed unused codes

--- app/experience/page.tsx
+++
@@ -1,9 +0,0 @@
-import ShellContainer from '@components/shell-container'
-
-export default function Experience() {
-  return (
-    <ShellContainer>
-      <h1>Work Experience</h1>
-    </ShellContainer>
-  )
-}
