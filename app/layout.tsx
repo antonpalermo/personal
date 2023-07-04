1f390723139a49490f67395fd75f415c19dc0d0a
anton palermo <anpbusinessmail@gmail.com>
2023-07-04 21:36:52 +0800

    refac: moved navbar to main layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,10 @@ import { cn } from '@lib/utils'
 import { Inter } from 'next/font/google'
 
 import { Heart } from 'lucide-react'
+import Brand from '@components/branding'
+import NavbarContainer from '@components/navbar-container'
+import SocialLinks from '@components/social-links'
+import { Button } from '@components/ui/button'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -22,9 +26,18 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={cn(inter.className, 'pt-8')}>
+        <div className="container mx-auto w-full md:w-6/12">
+          <nav className="inline-flex w-full items-center justify-between py-4">
+            <Brand />
+            <NavbarContainer>
+              <SocialLinks />
+              <Button size="sm">Contacts</Button>
+            </NavbarContainer>
+          </nav>
+        </div>
         {children}
         <footer className="py-8">
-          <div className="container mx-auto w-full md:w-6/12 pb-4">
+          <div className="container mx-auto w-full pb-4 md:w-6/12">
             <p className="w-full text-center">
               Created with <Heart className="inline-block h-4 w-4" /> by{' '}
               <strong>Anton Palermo</strong> &copy; {currentYear}
--- app/page.tsx
+++ app/page.tsx
@@ -11,13 +11,6 @@ import { expirences } from '@lib/experiences'
 export default function Home() {
   return (
     <main className="container mx-auto min-h-screen w-full md:w-6/12">
-      <nav className="inline-flex w-full items-center justify-between py-4">
-        <Brand />
-        <NavbarContainer>
-          <SocialLinks />
-          <Button size="sm">Contacts</Button>
-        </NavbarContainer>
-      </nav>
       <p className="py-5 leading-relaxed">
         Hola, it&apos;s nice that you stop by. I&apos;m a self tought web
         developer who loves working on my fullstack react projects.
