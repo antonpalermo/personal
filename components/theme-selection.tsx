12966fd657a551a9c5f918c7e78bb6522373b0c8
anton palermo <anpbusinessmail@gmail.com>
2023-07-09 22:52:45 +0800

    added theme icon

--- components/theme-selection.tsx
+++ components/theme-selection.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useTheme } from 'next-themes'
+import { Sun, Moon, Laptop } from 'lucide-react'
 
 import {
   DropdownMenu,
@@ -11,24 +12,35 @@ import {
   DropdownMenuTrigger
 } from '@components/ui/dropdown-menu'
 import { Button } from '@components/ui/button'
+import { useEffect, useState } from 'react'
 
 export default function ThemeSelection() {
   const { setTheme, theme } = useTheme()
+  const [mounted, setMounted] = useState<boolean>(false)
+
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted) {
+    return null
+  }
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="ghost">
-          {theme}
+        <Button variant="ghost" className="p-2">
+          {theme === 'dark' ? (
+            <Moon className="h-5 w-5" />
+          ) : (
+            <Sun className="h-5 w-5" />
+          )}
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent>
         <DropdownMenuLabel>Select Theme</DropdownMenuLabel>
         <DropdownMenuSeparator />
-        <DropdownMenuItem onClick={() => setTheme('system')}>
-          System
-        </DropdownMenuItem>
         <DropdownMenuItem onClick={() => setTheme('light')}>
           Light Mode
         </DropdownMenuItem>
