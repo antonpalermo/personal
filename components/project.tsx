e446851a9ceaa0a26330ae38d7dfb3f96721338f
antonpalermo <ANPBusinessmail@gmail.com>
2024-02-15 13:36:24 +0800

    update project list

--- components/project.tsx
+++ components/project.tsx
@@ -12,20 +12,23 @@ export interface ProjectProps {
 export default function Project({ project }: ProjectProps) {
   return (
     <div className="h-full rounded p-1 text-foreground shadow">
-      <div className="relative h-44 rounded">
+      <div className="relative h-52 rounded">
         <Image
           fill={true}
           src={project.source}
           alt={`${project.title} cover image`}
-          style={{ objectFit: 'cover' }}
+          style={{ objectFit: 'fill' }}
           quality={50}
           priority
         />
       </div>
       <div className="border-t border-slate-50 p-2">
-        <Link href={project.link} className="font-semibold">
-          {project.title}
-        </Link>
+        <div className="inline-flex w-full items-center justify-between">
+          <Link href={project.link} className="font-semibold">
+            {project.title}
+          </Link>
+          <p className="text-sm text-muted-foreground">{project.difficulty}</p>
+        </div>
         <p className="text-sm text-muted-foreground">{project.description}</p>
       </div>
     </div>
--- lib/projects.ts
+++ lib/projects.ts
@@ -3,28 +3,72 @@ export interface Project {
   description: string
   link: string
   source: string
+  difficulty: 'Basic' | 'Intermidiate' | 'Adavance'
 }
 
 export const projects: Project[] = [
   {
     title: 'Rick and Morty API',
+    difficulty: 'Intermidiate',
     description: 'Consumes Rick and Morty API using React Query with Next',
     link: 'https://rick-and-morty-app-psi.vercel.app/',
     source:
-      'https://res.cloudinary.com/dbndyylmv/image/upload/c_scale,h_180,q_auto:low,w_370/v1687189039/covers/rick_and_morty_app_ny33iz.jpg'
+      'https://res.cloudinary.com/dbndyylmv/image/upload/f_auto,q_auto/v1/covers/owvql89fhkyqjeyd7fde'
   },
   {
     title: 'Todo App',
+    difficulty: 'Intermidiate',
     description: 'Just another typical todo app built using Next with Tailwind',
     link: 'https://todo-app-silk-five.vercel.app/',
     source:
-      'https://res.cloudinary.com/dbndyylmv/image/upload/c_scale,h_180,q_auto:low,w_370/v1687447226/covers/todo-app_vdvgm2.jpg'
+      'https://res.cloudinary.com/dbndyylmv/image/upload/f_auto,q_auto/v1/covers/wocl1rnvuosjdeggwkwv'
   },
   {
     title: 'Airbnb Clone',
+    difficulty: 'Basic',
     description: 'React course simple airbnb clone app built using Vite.',
     link: 'https://airbnb-clone-alpha-three.vercel.app/',
     source:
-      'https://res.cloudinary.com/dbndyylmv/image/upload/c_scale,h_180,q_auto:low,w_370/v1687447480/covers/airbnb-clone_r85c4m.jpg'
+      'https://res.cloudinary.com/dbndyylmv/image/upload/f_auto,q_auto/v1/covers/jugldeptyu9o1nzwjw8m'
+  },
+  {
+    title: 'Travel Journal',
+    difficulty: 'Basic',
+    description: '',
+    link: 'https://travel-journal-virid.vercel.app/',
+    source:
+      'https://res.cloudinary.com/dbndyylmv/image/upload/f_auto,q_auto/v1/covers/ybnedebsjnnxxd9gk84m'
+  },
+  {
+    title: 'Business Card',
+    difficulty: 'Basic',
+    description: '',
+    link: 'https://business-card-roan.vercel.app/',
+    source:
+      'https://res.cloudinary.com/dbndyylmv/image/upload/f_auto,q_auto/v1/covers/v85ouuh7cggcztofrtoy'
+  },
+  {
+    title: 'Cat Profiles',
+    description: '',
+    difficulty: 'Basic',
+    link: 'https://contact-app-jade-ten.vercel.app/',
+    source:
+      'https://res.cloudinary.com/dbndyylmv/image/upload/f_auto,q_auto/v1/covers/ohcxyoetw9grlqyt45p7'
+  },
+  {
+    title: 'Ninja Listing',
+    difficulty: 'Intermidiate',
+    description: '',
+    link: 'https://contact-app-jade-ten.vercel.app/',
+    source:
+      'https://res.cloudinary.com/dbndyylmv/image/upload/f_auto,q_auto/v1/covers/xhaytlxlidbjb8hgdrsx'
+  },
+  {
+    title: 'React Facts',
+    difficulty: 'Basic',
+    description: '',
+    link: 'https://react-info-one.vercel.app/',
+    source:
+      'https://res.cloudinary.com/dbndyylmv/image/upload/f_auto,q_auto/v1/covers/arwxr72looizaagxsepf'
   }
 ]
