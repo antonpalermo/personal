d3569a80c974f822c194be3f6db98e6455a7c365
antonpalermo <ANPBusinessmail@gmail.com>
2024-02-17 00:00:17 +0800

    fix: build failed

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className="w-full space-y-5 sm:space-y-10">
       <p className="py-5 text-xl leading-relaxed">
-        Hello there 👋, thanks for stopping by! I'm Anton, a self-taught web
+        Hello there 👋, thanks for stopping by! I&apos;m Anton, a self-taught web
         developer specializing in JavaScript technologies, especially React and
         many more 💪!
       </p>
