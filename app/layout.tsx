af72179bbc888ded9d03d50dc12d6007df69ff53
antonpalermo <ANPBusinessmail@gmail.com>
2024-02-17 18:26:14 +0800

    added background

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,8 @@ import { Inter } from 'next/font/google'
 import Navbar from '@components/navbar'
 import Footer from '@components/footer'
 import ThemeProvider from '@components/theme/provider'
+import Image from 'next/image'
+import Background from '@components/background'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -22,6 +24,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <Background />
           <Navbar />
           <main className="mx-auto max-w-5xl px-10 sm:px-28">{children}</main>
           <Footer />
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,7 +1,29 @@
 import Image from 'next/image'
 
 import Heading from '@components/heading'
-import { projects } from '@lib/projects'
+import { projects, Project } from '@lib/projects'
+
+type ProjectProps = {
+  project: Project
+}
+
+function ProjectCard({ project }: ProjectProps) {
+  return (
+    <div className="h-auto w-full rounded border p-1">
+      <div className="relative min-h-80 rounded">
+        <Image
+          fill={true}
+          src={project.source}
+          alt={`${project.title} cover image`}
+          style={{ objectFit: 'cover', objectPosition: 'center' }}
+          quality={50}
+          priority
+          className="rounded"
+        />
+      </div>
+    </div>
+  )
+}
 
 export default function ProjectsPage() {
   return (
@@ -10,32 +32,9 @@ export default function ProjectsPage() {
         heading="My Projects"
         description="Collections of my completed projects so far."
       />
-      <div className="space-y-5 ">
+      <div className="grid-col-2 grid space-y-5">
         {projects.map(project => (
-          <div key={project.title} className="h-auto w-full rounded border p-1">
-            <div className="relative h-52 rounded">
-              <Image
-                fill={true}
-                src={project.source}
-                alt={`${project.title} cover image`}
-                style={{ objectFit: 'cover', objectPosition: 'center' }}
-                quality={50}
-                priority
-                className="rounded"
-              />
-            </div>
-            <div className="space-y-2 px-3 py-2">
-              <div className="flex w-full items-center justify-between">
-                <h2 className="font-medium">{project.title}</h2>
-                <span className="font-mono text-muted-foreground">
-                  {project.difficulty}
-                </span>
-              </div>
-              <p className="text-sm text-muted-foreground">
-                {project.description}
-              </p>
-            </div>
-          </div>
+          <ProjectCard key={project.title} project={project} />
         ))}
       </div>
     </div>
